0ec89ebaa0da20d5d2947b7943dfb9c4e58cc462
suresh4manpur <suresh4manpur@gmail.com>
2018-12-04 08:25:43 +0530

    Added BST, SynQueue and fairness implementation.

---
+++ kshare/src/com/kshare/qa/VolatileRefExample.java
@@ -0,0 +1,5 @@
+package com.kshare.qa;
+
+public class VolatileRefExample {
+
+}
---
+++ kshare/src/com/kshare/tree/Node.java
@@ -0,0 +1,21 @@
+package com.kshare.tree;
+
+public class Node{
+
+	int data;
+
+	Node left;
+
+	Node right;	
+
+	public Node(int data){
+
+		this.data = data;
+
+		left = null;
+
+		right = null;
+
+	}
+
+}
